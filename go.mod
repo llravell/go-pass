1e8465c61cc9ef6202f2c9e2d38dcce8bb74d3a6
suraginv <suraginv@yandex-team.ru>
2025-04-09 20:15:32 +0300

    tests: auth

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/pressly/goose/v3 v3.24.1
 	github.com/rs/zerolog v1.34.0
 	github.com/stretchr/testify v1.10.0
+	go.uber.org/mock v0.5.1
 	golang.org/x/crypto v0.33.0
 	google.golang.org/grpc v1.71.0
 	google.golang.org/protobuf v1.36.4
@@ -46,7 +47,7 @@ require (
 	github.com/urfave/cli/v3 v3.0.0-beta1
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
-	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sync v0.11.0
 	golang.org/x/sys v0.30.0 // indirect
 	modernc.org/libc v1.61.13 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
---
+++ internal/grpc/server/auth_test.go
@@ -0,0 +1,214 @@
+package server_test
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	"testing"
+
+	"github.com/llravell/go-pass/internal/entity"
+	"github.com/llravell/go-pass/internal/grpc/server"
+	"github.com/llravell/go-pass/internal/mocks"
+	usecase "github.com/llravell/go-pass/internal/usecase/server"
+	pb "github.com/llravell/go-pass/pkg/grpc"
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/test/bufconn"
+)
+
+func encryptUserPassword(t *testing.T, raw string) string {
+	t.Helper()
+
+	passwordBytes, err := bcrypt.GenerateFromPassword([]byte(raw), bcrypt.DefaultCost)
+	require.NoError(t, err)
+
+	return string(passwordBytes)
+}
+
+func startGRPCAuthServer(
+	t *testing.T,
+	userRepo usecase.UserRepository,
+	jwtIssuer usecase.JWTIssuer,
+) (pb.AuthClient, func()) {
+	t.Helper()
+
+	logger := zerolog.Nop()
+	authUsecase := usecase.NewAuthUseCase(userRepo, jwtIssuer)
+	authServer := server.NewAuthServer(authUsecase, &logger)
+
+	lis := bufconn.Listen(bufSize)
+	server := grpc.NewServer()
+	pb.RegisterAuthServer(server, authServer)
+
+	go func() {
+		if err := server.Serve(lis); err != nil {
+			log.Fatalf("Server exited with error: %v", err)
+		}
+	}()
+
+	conn, err := grpc.NewClient(
+		"passthrough://bufnet",
+		grpc.WithContextDialer(func(_ context.Context, _ string) (net.Conn, error) {
+			return lis.Dial()
+		}),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	require.NoError(t, err)
+
+	closeFn := func() {
+		conn.Close()
+		server.Stop()
+	}
+
+	return pb.NewAuthClient(conn), closeFn
+}
+
+func TestAuthServer_Register(t *testing.T) {
+	userRepo := mocks.NewMockUserRepository(gomock.NewController(t))
+	jwtIssuer := mocks.NewMockJWTIssuer(gomock.NewController(t))
+
+	client, closeFn := startGRPCAuthServer(t, userRepo, jwtIssuer)
+
+	defer closeFn()
+
+	t.Run("register new user", func(t *testing.T) {
+		userRepo.EXPECT().
+			StoreUser(gomock.Any(), "login", gomock.Any()).
+			Return(&entity.User{ID: 1, Login: "login"}, nil)
+
+		jwtIssuer.EXPECT().
+			Issue(1, gomock.Any()).
+			Return("token", nil)
+
+		response, err := client.Register(t.Context(), &pb.AuthRequest{
+			Login:    "login",
+			Password: "pass",
+		})
+		require.NoError(t, err)
+
+		assert.Equal(t, "token", response.Token)
+	})
+
+	t.Run("already exists error", func(t *testing.T) {
+		userRepo.EXPECT().
+			StoreUser(gomock.Any(), "login", gomock.Any()).
+			Return(nil, entity.ErrUserConflict)
+
+		_, err := client.Register(t.Context(), &pb.AuthRequest{
+			Login:    "login",
+			Password: "pass",
+		})
+
+		status, ok := status.FromError(err)
+		require.True(t, ok)
+
+		assert.Equal(t, codes.AlreadyExists, status.Code())
+	})
+
+	t.Run("user storing error", func(t *testing.T) {
+		userRepo.EXPECT().
+			StoreUser(gomock.Any(), "login", gomock.Any()).
+			Return(nil, errors.New("Boom!"))
+
+		_, err := client.Register(t.Context(), &pb.AuthRequest{
+			Login:    "login",
+			Password: "pass",
+		})
+
+		status, ok := status.FromError(err)
+		require.True(t, ok)
+
+		assert.Equal(t, codes.Unknown, status.Code())
+	})
+
+	t.Run("token issuing error", func(t *testing.T) {
+		userRepo.EXPECT().
+			StoreUser(gomock.Any(), "login", gomock.Any()).
+			Return(&entity.User{ID: 1, Login: "login"}, nil)
+
+		jwtIssuer.EXPECT().
+			Issue(1, gomock.Any()).
+			Return("", errors.New("Boom!"))
+
+		_, err := client.Register(t.Context(), &pb.AuthRequest{
+			Login:    "login",
+			Password: "pass",
+		})
+
+		status, ok := status.FromError(err)
+		require.True(t, ok)
+
+		assert.Equal(t, codes.Unknown, status.Code())
+	})
+}
+
+func TestAuthServer_Login(t *testing.T) {
+	userRepo := mocks.NewMockUserRepository(gomock.NewController(t))
+	jwtIssuer := mocks.NewMockJWTIssuer(gomock.NewController(t))
+
+	client, closeFn := startGRPCAuthServer(t, userRepo, jwtIssuer)
+
+	defer closeFn()
+
+	t.Run("login user", func(t *testing.T) {
+		userRepo.EXPECT().
+			FindUserByLogin(gomock.Any(), "login").
+			Return(&entity.User{ID: 1, Login: "login", Password: encryptUserPassword(t, "pass")}, nil)
+
+		jwtIssuer.EXPECT().
+			Issue(1, gomock.Any()).
+			Return("token", nil)
+
+		response, err := client.Login(t.Context(), &pb.AuthRequest{
+			Login:    "login",
+			Password: "pass",
+		})
+		require.NoError(t, err)
+
+		assert.Equal(t, "token", response.Token)
+	})
+
+	t.Run("user fetching error", func(t *testing.T) {
+		userRepo.EXPECT().
+			FindUserByLogin(gomock.Any(), "login").
+			Return(nil, errors.New("Boom!"))
+
+		_, err := client.Login(t.Context(), &pb.AuthRequest{
+			Login:    "login",
+			Password: "pass",
+		})
+
+		status, ok := status.FromError(err)
+		require.True(t, ok)
+
+		assert.Equal(t, codes.Unknown, status.Code())
+	})
+
+	t.Run("token issuing error", func(t *testing.T) {
+		userRepo.EXPECT().
+			FindUserByLogin(gomock.Any(), "login").
+			Return(&entity.User{ID: 1, Login: "login", Password: encryptUserPassword(t, "pass")}, nil)
+
+		jwtIssuer.EXPECT().
+			Issue(1, gomock.Any()).
+			Return("", errors.New("Boom!"))
+
+		_, err := client.Login(t.Context(), &pb.AuthRequest{
+			Login:    "login",
+			Password: "pass",
+		})
+
+		status, ok := status.FromError(err)
+		require.True(t, ok)
+
+		assert.Equal(t, codes.Unknown, status.Code())
+	})
+}
